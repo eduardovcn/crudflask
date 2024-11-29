975d1e3907710333eb68943c57632c92f95222dd
Eduardo Vitor <eduardovcn74@gmail.com>
2024-11-29 00:11:25 -0300

    Adding test & Updating the create endpoints

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def create_task():
     task_id_control += 1
     print(tasks)
     print(data)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route("/tasks", methods=["GET"]) #Listar todas as tarefas
 def get_tasks():
